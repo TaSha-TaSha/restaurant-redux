2d8e88aaba159f63f9254a8bd474560e43815ea9
塔莎 TaSha 塔莎 <96839389+TaSha-TaSha@users.noreply.github.com>
2022-11-05 22:24:23 +0900

    Update Filter.js

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -10,13 +10,3 @@ const Filter = ({category}) => {
 }
 
 export default Filter;
-
-
-// import { useSelector } from "react-redux"; это ХУК который нам нужен, чтобы прочитать состояние
-// import { getSelectedCategory } from '../../redux/dishesSlice'; из dishesSlece
-// className скобки фигурные {} вместо '' и для CSS прописываем требования
-// после того как мы добавим CSS на кнопки , у нас черный экран и говорит, что Provider не найден. Значит, мы должны добавить в index.js
-// после этого все равно экран черный, говорит, что selectedCategory is not defined это значит, что
-// нужно в store.js reduser: dishes и интегрировать from dishesSlice
-// первоначально состояние мы меняем в dishesSlice в initialState: {selectedCategory: 'SEAFOOD}
-// используем хук dispatch также импотритуем filtercategory
