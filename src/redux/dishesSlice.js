ef9601b77bf8ee6263afa6d79b1694a6bfb18d4a
塔莎 TaSha 塔莎 <96839389+TaSha-TaSha@users.noreply.github.com>
2022-11-05 22:26:55 +0900

    Update dishesSlice.js

--- src/redux/dishesSlice.js
+++ src/redux/dishesSlice.js
@@ -1,5 +1,3 @@
-// reducers ЭТО ИНСТРУКЦИЯ ДЛЯ ВСЕХ КОМПОНЕНТОВ !!!!!!!!!!!!!!
-
 import { createSlice } from '@reduxjs/toolkit';
 
 export const dishesSlice = createSlice( {
@@ -19,45 +17,3 @@ export const { filterCategory } = dishesSlice.actions;
 export default dishesSlice.reducer
 
 
-
-
-// здесь мы будем говорить про разные категории
-// если выбрана эта категория, тогда поменяй цвет
-// если не выбрана эта категория, тогда оставь дркгой цвет
-// initialState - это первоначальное наше состояние
-// reducers - пока оставляем незаполненный, заполним позже
-
-
-
-// Create a Redux State Slice
-// позволяет нам сначало инициировать наше состояние, то есть сказать чему равно наше первоначальное состояние, а потом его определенным образом менять
-// reducers то есть как мы будем менять что-либо, если наши действия совершены
-
-
-// initialState: {
-//     selectedCategory: 'SEAFOOD'
-// }, здесь мы хотим, чтобы когда загружалась наша страница, в первую очередь загружалась выбранная категория 'SEAFOOD', прописываем здесь НО применять будем в другом документе
-
-// export default dishesSlice.reducer это запоминаем, это такие настройки
-
-// export const getSelectedCategory = state => state.dishes.selectedCategory;
-// здесь создаем const getSelectedCategory приравниваем к state к первоначальному состоянию, далее к name: 'dishes' далее к первоначальному состоянию к initialState: selectedCategory: 'SEAFOOD'
-
-// далее переходим в filter так как там прописываем category и там импортируем этот компонент
-
-// после filter мы продолжаем заполнять dishesSlice reducers и говорим, что
-// у нас будет другой компонент в котором мы изменем состояние через action.paylod
-// незабываем export const { filterCategory } = dishesSlice.actions; чтобы использовать в другом компоненте
-// filterCategory название мы дали сами
-
-// reducers: {
-//     filterCategory: (state, action) => {
-//         state.selectedCategory = action.payload;
-//     }
-
-// то есть как заполнять этот компонент мы берем инструкцию в Create a Redux State Slice
-
-
-
-
-// reducers ЭТО ИНСТРУКЦИЯ ДЛЯ ВСЕХ КОМПОНЕНТОВ !!!!!!!!!!!!!!
